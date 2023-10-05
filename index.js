9a354619886e0b597963ab67ad6ca7a927cacdbc
Subhakanta Das <127312896+Mrsiku9@users.noreply.github.com>
2023-10-05 19:27:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const __dirname = path.resolve()
 
 const app = express();
 const port = process.env.PORT ||8000;
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 app.use(cors());
