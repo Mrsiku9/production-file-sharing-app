81c975a083851414bf97c0b9fc5eed88eabaa63b
siku97 <subhakantadas452@gmail.com>
2023-10-05 17:11:11 +0530

    final code
    mern app deployment

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ import Route from "./routes/route.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import ConnectDB from "./database/db.js";
+import path from "path"
+const __dirname = path.resolve()
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT ||8000;
+app.use(express.static(path.join(__dirname, "./client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+});
+
 app.use(cors());
 app.use("/", Route);
+
 dotenv.config();
 
 const username = process.env.DB_USERNAME;
